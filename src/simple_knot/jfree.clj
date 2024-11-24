a8ef91b3de5641cf6deb6d9e79493bc37f12dbeb
Marcin Nowakowski <game.of.no@gmail.com>
2024-11-24 17:43:19 +0100

    #2 Adding JFreeChart knots.

---
+++ src/simple_knot/jfree.clj
@@ -0,0 +1,69 @@
+(ns simple-knot.jfree
+  (:gen-class)
+  (:import [org.jfree.chart ChartPanel JFreeChart]
+           [org.jfree.chart.axis NumberAxis]
+           [org.jfree.chart.plot XYPlot]
+           [org.jfree.chart.renderer.xy XYBlockRenderer]
+           [org.jfree.data.xy DefaultXYZDataset]
+           [javax.swing JFrame]
+           [org.jfree.chart.renderer LookupPaintScale]
+           [java.awt Color]))
+
+(defn create-heatmap []
+  ;; Define ASCII data as a grid
+  (let [ascii-data [["0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "3" "2" "2" "2" "2" "2" "2" "1"]
+                    ["0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "3" "0" "0" "0" "0" "0" "0" "1"]
+                    ["2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "3" "2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "1"]
+                    ["0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "3" "0" "0" "0" "0" "0" "0" "1" "0" "0" "0" "0" "0" "1"]
+                    ["0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "3" "0" "0" "0" "0" "0" "0" "3" "3" "3" "3" "3" "3" "3" "3" "3" "3" "3" "3" "3" "3"]
+                    ["0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "3" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "1"]
+                    ["0" "0" "0" "0" "0" "0" "0" "0" "0" "0" "3" "2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "2" "1"]]
+        ;; Map characters to numeric values
+        char-to-value {"0" 0
+                       "1" 1
+                       "2" 2
+                       "3" 3}
+        ;; Process ASCII data into XYZ format
+        xyz-data (for [y (range (count ascii-data))
+                       x (range (count (nth ascii-data y)))
+                       :let [char (get (nth ascii-data y) x "0")]]
+                   [(double x) (double (- (count ascii-data) 1 y)) (double (get char-to-value char 0))])
+        ;; Separate XYZ data into individual arrays
+        x-values (double-array (map first xyz-data))
+        y-values (double-array (map second xyz-data))
+        z-values (double-array (map last xyz-data))
+        dataset (DefaultXYZDataset.)]
+    ;; Create the 2D array for the dataset
+    (let [series-data (make-array Double/TYPE 3 (count x-values))]
+      (aset series-data 0 x-values)
+      (aset series-data 1 y-values)
+      (aset series-data 2 z-values)
+      (.addSeries dataset "Heatmap" series-data))
+
+    ;; Create a paint scale
+    (let [paint-scale (LookupPaintScale. 0.0 3.0 Color/WHITE)
+          _ (doseq [[value color] [[1 Color/GREEN]
+                                   [2 Color/YELLOW]
+                                   [3 Color/RED]]]
+              (.add paint-scale value color))
+          renderer (doto (XYBlockRenderer.)
+                     (.setPaintScale paint-scale))
+          x-axis (NumberAxis. "X")
+          y-axis (NumberAxis. "Y")
+          plot (doto (XYPlot. dataset x-axis y-axis renderer)
+                 (.setDomainPannable true)
+                 (.setRangePannable true))
+          chart (doto (JFreeChart. "Heatmap Example"
+                                   JFreeChart/DEFAULT_TITLE_FONT
+                                   plot true)
+                  (.removeLegend))]
+
+      ;; Display the chart in a JFrame
+      (doto (JFrame. "Heatmap Example")
+        (.setContentPane (ChartPanel. chart))
+        (.setSize 800 600)
+        (.setVisible true)))))
+
+;; Main function
+(defn -main [& args]
+  (create-heatmap))
